0592e8e58f7c2cc511a43deb92648202bc3a8ef5
Raymond Pang <124433610+pppkkkkkkk@users.noreply.github.com>
2025-09-22 15:08:13 +0000

    Updated

--- code.py
+++ code.py
@@ -1,14 +1,26 @@
 
 class Solution:
     def countWays(self, n):
-        # code here
-        if n <= 1:
-            return n
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        # # code here
+        # if n <= 1:
+        #     return n
+        # dp = [0]*(n+1)
+        # dp[1] = 1
+        # dp[2] = 2
+        # for i in range(3,n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
         
-        return dp[n]
+        # return dp[n]
             
+        dp = {}
+        def recur(n, dp):
+            if n <= 2:
+                dp[n] = n
+                return n
+            if n in dp:
+                return dp[n]
+            ans = recur(n-1, dp) + recur(n-2, dp)
+            dp[n] = ans 
+            return ans
+            
+        return recur(n, dp)
